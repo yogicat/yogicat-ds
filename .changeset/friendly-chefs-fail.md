c1a3d60785495e8b91a8c4e3e82a42258146aa07
Dahye Oh <dahe.oh@gmail.com>
2021-08-10 14:11:47 +0900

    fix: button text

---
+++ .changeset/friendly-chefs-fail.md
@@ -0,0 +1,5 @@
+---
+'button': patch
+---
+
+fix button text
--- packages/button/src/button.tsx
+++ packages/button/src/button.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default function Button() {
-  return <div>cool button</div>
+  return <div>I am a cool button</div>
 }
