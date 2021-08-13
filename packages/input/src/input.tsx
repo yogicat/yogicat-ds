0943dd261c73eb4fd8d5b89b53ec6ff5f7778833
Dahye Oh <dahe.oh@gmail.com>
2021-08-13 14:52:44 +0900

    fix(input): fix label text

--- packages/input/src/input.tsx
+++ packages/input/src/input.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Input = () => {
   return (
     <label>
-      cool input
+      Cool input label
       <input type="text" name="text" required />
     </label>
   )
